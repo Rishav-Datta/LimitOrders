55819d30c6e6bdd889ccaf08df6e8183726cc52f
Rishav-Datta <rishiedatta@gmail.com>
2024-09-06 12:03:31 +0530

    Update LimitOrderAgentTest.java

--- limit_orders/src/test/java/org/afob/limit/LimitOrderAgentTest.java
+++ limit_orders/src/test/java/org/afob/limit/LimitOrderAgentTest.java
@@ -1,10 +1,89 @@
 package org.afob.limit;
 
+import org.afob.execution.ExecutionClient;
+import org.afob.execution.ExecutionClient.ExecutionException;
+import org.afob.limit.LimitOrderAgent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.math.BigDecimal;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
 public class LimitOrderAgentTest {
 
+    private ExecutionClient mockExecutionClient;
+    private LimitOrderAgent limitOrderAgent;
+
+    @BeforeEach
+    public void setUp(){
+        mockExecutionClient = Mockito.mock(ExecutionClient.class);
+        limitOrderAgent = new LimitOrderAgent(mockExecutionClient);
+    }
+
+    @Test
+    public void testBuyOrderExecutedWhenPriceDropsBelowLimit() throws ExecutionException{
+        limitOrderAgent.addOrder(true, "IBM", 1000, BigDecimal.valueOf(100));
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(99));
+        verify(mockExecutionClient, times(1)).buy("IBM", 1000);
+    }
+
+    @Test
+    public void testBuyOrderNotExecutedWhenPriceAboveLimit() throws ExecutionException{
+        limitOrderAgent.addOrder(true, "IBM", 1000, BigDecimal.valueOf(100));
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(101));
+        verify(mockExecutionClient, never()).buy("IBM", 1000);
+    }
+
+    @Test
+    public void testSellOrderExecutedWhenPriceRisesAboveLimit() throws ExecutionException{
+        limitOrderAgent.addOrder(false, "IBM", 1000, BigDecimal.valueOf(150));
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(151));
+        verify(mockExecutionClient, times(1)).sell("IBM", 1000);
+    }
+
+    @Test
+    public void testSellOrderNotExecutedWhenPriceBelowLimit() throws ExecutionException{
+        limitOrderAgent.addOrder(false, "IBM", 1000, BigDecimal.valueOf(150));
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(149));
+        verify(mockExecutionClient, never()).sell("IBM", 1000);
+    }
+
+    @Test
+    public void testOrderIsExecutedAndThenRemoved() throws ExecutionException{
+        limitOrderAgent.addOrder(true, "IBM", 1000, BigDecimal.valueOf(100));
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(99));
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(98));
+        verify(mockExecutionClient, times(1)).buy("IBM", 1000);
+    }
+
+
+     @Test
+    public void testMultipleOrdersExecuted() throws ExecutionException{
+        limitOrderAgent.addOrder(true, "IBM", 500, BigDecimal.valueOf(100));
+        limitOrderAgent.addOrder(false, "IBM", 200, BigDecimal.valueOf(150));
+        
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(99));
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(151));
+        
+        verify(mockExecutionClient, times(1)).buy("IBM", 500);
+        verify(mockExecutionClient, times(1)).sell("IBM", 200);
+    }
+
+    @Test
+    public void testExecutionExceptionHandling() throws ExecutionException{
+        limitOrderAgent.addOrder(true, "IBM", 1000, BigDecimal.valueOf(100));
+        doThrow(new ExecutionException("Failed to buy")).when(mockExecutionClient).buy("IBM", 1000);
+        
+        limitOrderAgent.priceTick("IBM", BigDecimal.valueOf(99));
+        
+        verify(mockExecutionClient, times(1)).buy("IBM", 1000);
+    }
+    
     @Test
     public void addTestsHere() {
         Assert.fail("not implemented");
